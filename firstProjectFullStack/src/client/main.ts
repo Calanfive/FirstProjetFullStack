eb135a06a4dcdaeea632d1236b2f2a973788fb9d
calanfive <camille.lelan@gmail.com>
2023-09-06 16:42:22 +0200

    fetch+params

--- firstProjectFullStack/src/client/main.ts
+++ firstProjectFullStack/src/client/main.ts
@@ -1,27 +1,3 @@
-import "./style.css";
-
-import { setupCounter } from "./counter";
-import typescriptLogo from "./typescript.svg";
-
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`;
-
-setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
 
 // création champs de saisie + button
 const app = document.querySelector("#app") as HTMLDivElement
@@ -38,22 +14,23 @@ button.style.color = "white";
 app.appendChild(input);
 app.appendChild(button);
 
-button.addEventListener("click", () => {
+button.addEventListener("click",  async () => {
   if(input.value.length > 0){
     console.log(input.value);
+    const res = await fetch("/hello/" + input.value)
+    const message = await res.text()
     
     const resultAPI = document.createElement('p') 
     resultAPI.classList.add('result');
     resultAPI.style.backgroundColor = 'green'
-    resultAPI.innerText = input.value;
+    resultAPI.style.color = "white";
+    resultAPI.innerText = message;
       
     app.appendChild(resultAPI);
-    }
-
     input.value = '';
-
-  // else {
-  //   alert("Rentrer au moins un caractère")
-  // }
+  }
+  else {
+    alert("Rentrer au moins un caractère")
+  }
 
 })
